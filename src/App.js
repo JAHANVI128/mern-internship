3d5964ca499719c1702198340300b0c7ae0b2412
Jahanvi <jahanvimp128@gmail.com>
2023-08-02 15:32:55 +0530

    components loaded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import { Footer } from './components/Footer';
 
 function App() {
+
+  var name = "Ajit";
+  var age = 26;
+  var isMale = true;
+  var marks = 8.9;
+
+  var user = {
+    name: "Harsh",
+    age: 28,
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div style={{ backgroundColor: "green" ,padding:"100px"}}>
+
+        <h1>Ajay</h1>
+        <h1>{name}</h1>
+        <h2>Age = {age}</h2>
+        <h3>{isMale ? "male" : "female"}</h3>
+        {user.name}<br />
+        {user.age}
+
+      </div>
+        <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Footer = () => {
+
+    var footerStyle = {
+        position: "relative",
+        width:"100%",
+        border:"2px solid black"
+    }
+  return (
+    <div style={footerStyle}>
+        <h1>Footer</h1>
+    </div>
+  )
+}
