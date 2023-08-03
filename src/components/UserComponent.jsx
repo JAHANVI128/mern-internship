87d7560e99f3679cb4628d56df27ea1943b3ae4e
Jahanvi <jahanvimp128@gmail.com>
2023-08-03 15:27:47 +0530

    usestate

--- src/components/UserComponent.jsx
+++ src/components/UserComponent.jsx
@@ -1,47 +1,78 @@
-import React from 'react'
-import { UserList } from './UserList'
+import React, { useState } from "react";
+import { UserList } from "./UserList";
 
 export const UserComponent = (props) => {
-
   const test = (user) => {
     // alert("test => "+name)
 
-    alert("test method called...")
-    console.log("test method called",user)
-  }
+    alert("test method called...");
+    console.log("test method called", user);
+  };
 
   const deleteUser = (id) => {
-    console.log("user deleted...",id)
+    console.log("user deleted...", id);
     users = users.filter((user) => {
-      return user.id != id
-    })
+      return user.id != id;
+    });
 
-    console.log("after delete...",users)
+    setusers(users)
+    console.log("after delete...", users);
+  };
+
+  const addUser = (user) => {
+    console.log("add user called...",user)
+
+    users = [...users,user]
+    setusers(users)
   }
-  console.log(props)
 
-  var users = [{
-    id: 1,
-    name:"Mahima",
-    age:23
-  },{
-    id :2,
-    name:"Jenil",
-    age:25    
-  },
-{
-  id : 3,
-  name: "Dixen",
-  age:26
-}]
+  console.log(props);
+
+  //   var users = [{
+  //     id: 1,
+  //     name:"Mahima",
+  //     age:23
+  //   },{
+  //     id :2,
+  //     name:"Jenil",
+  //     age:25
+  //   },
+  // {
+  //   id : 3,
+  //   name: "Dixen",
+  //   age:26
+  // }]
+
+  var [users, setusers] = useState([
+    {
+      id: 1,
+      name: "Mahima",
+      age: 23,
+    },
+    {
+      id: 2,
+      name: "Jenil",
+      age: 25,
+    },
+    {
+      id: 3,
+      name: "Dixen",
+      age: 26,
+    },
+  ]);
+
   return (
     <div>
       <h1>{props.title}</h1>
-        <h1>
-            User Component
-        </h1>
-        {/* <button onClick={() => {test()}}>Test</button> */}
-        <UserList apptitle = {props.title} users = {users} test = {test} delete = {deleteUser}/>
+      <h1>User Component</h1>
+      {/* <button onClick={() => {test()}}>Test</button> */}
+      <UserList
+        apptitle={props.title}
+        users={users}
+        test={test}
+        delete={deleteUser}
+        add = {addUser}
+      />
     </div>
-  )
-}
+  );
+};
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,37 +1,40 @@
-import React from 'react'
+import React from "react";
 
 export const UserList = (props) => {
+  console.log("user list ....", props);
 
-    console.log("user list ....",props)
+  var userObj = {
+    id:4,
+    name:"Rohan",
+    age:20
+  }
+
   return (
     <div>
-        <h3>User List</h3>
-        <table className='table'>
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>Age</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    props.users.map((user) => {
-                        return(
-                            <tr>
-                                <td>{user.id}</td>
-                                <td>{user.name}</td>
-                                <td>{user.age}</td>
-                                <td>
-                                    <button className='btn btn-danger' onClick= { () => props.delete(user.id)}>Delete</button>
-                                </td>
-                            </tr>
-                        )
-                    })
-                }
-            </tbody>
-        </table>
+      <h3>User List</h3>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.users.map((user) => {
+            return (
+              <tr>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.age}</td>
+                <td><button className="btn btn-danger" onClick={() => props.delete(user.id)}>Delete</button></td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <button className="btn btn-primary" onClick={() => {props.add(userObj)}}>ADD</button>
     </div>
-  )
-}
+  );
+};
